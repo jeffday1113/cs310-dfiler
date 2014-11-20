c5365c3e6e61db20801c2d04d0656f0d4a81728f
Andrew Gauthier <ajg45@duke.edu>
2014-11-20 01:28:42 -0500

    inode started

--- src/dfs/DFile.java
+++ src/dfs/DFile.java
@@ -10,12 +10,26 @@ public class DFile {
 	private List<Integer> myBlocks;
 	private int myNumReaders;
 	private int myNumWriters;
+	private int size;
 	
 	public DFile (DFileID fileID, List<Integer> blocks){
 		myFileID=fileID;
 		myBlocks=blocks;
 		myNumReaders=0;
 		myNumWriters=0;
+		size = blocks.size();
+	}
+	
+	public int size(){
+		return size;
+	}
+	
+	public DFileID id(){
+		return myFileID;
+	}
+	
+	public List<Integer> getBlocks(){
+		return myBlocks;
 	}
 
 }
--- src/dfs/INode.java
+++ src/dfs/INode.java
@@ -1,5 +1,27 @@
 package dfs;
+import java.nio.ByteBuffer;
+
+import common.Constants;
 
 public class INode {
+	
+	public byte[] makeByteArrayInodeFromDFile(DFile d){
+		byte[] ret = new byte[ Constants.INODE_SIZE ];
+		byte[] sizeBytes = ByteBuffer.allocate(4).putInt(d.size()).array();
+		byte[] idBytes = ByteBuffer.allocate(4).putInt(d.id().getDFileID()).array();
+		for(int i = 0; i < 4; i++){
+			ret[i] = sizeBytes[i];
+			ret[i+4] = idBytes[i];
+		}
+		int pos = 8;
+		for(Integer i:d.getBlocks()){
+			byte[] mapBytes = ByteBuffer.allocate(4).putInt(i).array();
+			for(int j = 0; j < 4; j++){
+				ret[j + pos] = mapBytes[j];
+			}
+			pos = pos+4;
+		}
+		return ret;
+	}
 
 }
