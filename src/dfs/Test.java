1ea1e0bf71d09b66b756c0706b9399e21f8bb29f
ajg45 <ajg45@duke.edu>
2014-11-23 18:55:45 -0500

    Update Test.java

--- src/dfs/Test.java
+++ src/dfs/Test.java
@@ -1,11 +1,23 @@
 package dfs;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import common.DFileID;
 
 public class Test {
 
 	public static void main(String arg[]){
 		MyDFS dfs = new MyDFS();
+		try {
+			dfs.init();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		DFileID j = dfs.createDFile();
 	}
 	
